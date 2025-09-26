e92f58361e9dd9e846107ed948c0d65bd1d147c5
tbreuss <thomasbreuss@gmx.ch>
2025-09-26 11:19:03 +0200

    Disable SPA

--- quartz.config.ts
+++ quartz.config.ts
@@ -10,7 +10,7 @@ const config: QuartzConfig = {
   configuration: {
     pageTitle: "Thomas' Notes",
     pageTitleSuffix: "",
-    enableSPA: true,
+    enableSPA: false,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
